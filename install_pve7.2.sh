c81ef0c937c838cd535220e7ab92ad9e63297e95
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-13 17:30:33 +0800

    Update install_pve7.2.sh

--- install_pve7.2.sh
+++ install_pve7.2.sh
@@ -7,8 +7,13 @@ if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
+if ! command -v wget > /dev/null 2>&1; then
+      apt-get install -y wget
+fi
 echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 wget http://download.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
+apt-get update
+apt-get install debian-keyring debian-archive-keyring -y
 apt-get update
 apt-get install proxmox-ve=7.2-1
 rm /etc/apt/sources.list.d/pve-install-repo.list
