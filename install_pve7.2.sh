800154313ff414276b85f4e3e3e40b97dff82b29
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-13 17:38:03 +0800

    Update install_pve7.2.sh

--- install_pve7.2.sh
+++ install_pve7.2.sh
@@ -10,14 +10,16 @@ fi
 if ! command -v wget > /dev/null 2>&1; then
       apt-get install -y wget
 fi
-echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+# echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" >> /etc/apt/sources.list
 wget http://download.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
-wget http://download.proxmox.com/debian/proxmox-release-buster.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-buster.gpg
-apt-key add /etc/apt/trusted.gpg.d/proxmox-release-buster.gpg
 apt-key add /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
+# wget http://download.proxmox.com/debian/proxmox-release-buster.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-buster.gpg
+# apt-key add /etc/apt/trusted.gpg.d/proxmox-release-buster.gpg
 apt-get update
 apt-get install debian-keyring debian-archive-keyring -y
+apt-get autoremove
 apt-get update
 apt-get install proxmox-ve=7.2-1
 rm /etc/apt/sources.list.d/pve-install-repo.list
-echo "Proxmox VE 7.2 has been successfully installed."
+
