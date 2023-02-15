1bf6a7387b2a45aeaf5f79fa2b6850d6845390d0
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-15 15:02:15 +0800

    Update check_kernal.sh

--- check_kernal.sh
+++ check_kernal.sh
@@ -3,20 +3,20 @@
 
 # 检查CPU是否支持硬件虚拟化
 if [ "$(egrep -c '(vmx|svm)' /proc/cpuinfo)" -eq 0 ]; then
-    echo "ERROR: CPU does not support hardware virtualization"
+    echo "CPU不支持硬件虚拟化"
 fi
 
 # 检查虚拟化选项是否启用
 if [ "$(grep -E -c '(vmx|svm)' /proc/cpuinfo)" -eq 0 ]; then
-    echo "ERROR: Hardware virtualization is not enabled in the BIOS"
+    echo "BIOS中未启用硬件虚拟化"
 fi
 
 # 检查KVM模块是否已加载
 if lsmod | grep -q kvm; then
-    echo "KVM module is already loaded"
+    echo "KVM模块已经加载"
 else
     # 加载KVM模块并将其添加到/etc/modules文件中
     modprobe kvm
     echo "kvm" >> /etc/modules
-    echo "KVM module has been loaded and added to /etc/modules"
+    echo "KVM模块已加载并添加到 /etc/modules"
 fi
