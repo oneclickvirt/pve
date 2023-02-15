6ee3daa9614636260cc18afd75596f7eb0954308
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-15 15:34:21 +0800

    Update check_kernal.sh

--- check_kernal.sh
+++ check_kernal.sh
@@ -3,12 +3,14 @@
 
 # 检查CPU是否支持硬件虚拟化
 if [ "$(egrep -c '(vmx|svm)' /proc/cpuinfo)" -eq 0 ]; then
-    echo "CPU不支持硬件虚拟化"
+    echo "CPU不支持硬件虚拟化，无法嵌套虚拟化KVM服务器"
+    exit 1
 fi
 
 # 检查虚拟化选项是否启用
 if [ "$(grep -E -c '(vmx|svm)' /proc/cpuinfo)" -eq 0 ]; then
-    echo "BIOS中未启用硬件虚拟化"
+    echo "BIOS中未启用硬件虚拟化，无法嵌套虚拟化KVM服务器"
+    exit 1
 fi
 
 # 检查KVM模块是否已加载
