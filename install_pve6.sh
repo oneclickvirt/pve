f7324a052c0f17f95bda83cd83839241aa9849ee
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-15 10:51:51 +0800

    Update install_pve6.sh

--- install_pve6.sh
+++ install_pve6.sh
@@ -56,7 +56,9 @@ apt -y install proxmox-ve postfix open-iscsi
 
 # 检测是否安装成功
 url="https://${ip}:8006/"
-if curl --output /dev/null --silent --head --fail "$url"; then
+echo "安装完毕，请打开HTTPS网页 {$url}"
+echo "用户名、密码就是服务器所使用的用户名、密码"
+if curl --output /dev/null --silent --insecure --head --fail "$url"; then
   echo "安装成功"
 else
   echo "安装失败"
