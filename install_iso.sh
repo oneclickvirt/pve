195010f1a2c1653557d41f111706df9286178355
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-15 12:47:35 +0800

    Update install_iso.sh

--- install_iso.sh
+++ install_iso.sh
@@ -3,8 +3,8 @@
 
 # 下载Ubuntu和Debian的最新系统镜像
 wget -P /root/ https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
-wget -P /root/ https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.1.0-amd64-netinst.iso
+wget -P /root/ https://github.com/spiritLHLS/pve/releases/download/debian-11.6.0-amd64-netinst.iso/debian-11.6.0-amd64-netinst.iso
 
 # 将镜像文件移动到Proxmox VE的模板目录中
 mv /root/focal-server-cloudimg-amd64.img /var/lib/vz/template/iso/
-mv /root/debian-11.1.0-amd64-netinst.iso /var/lib/vz/template/iso/
+mv /root/debian-11.6.0-amd64-netinst.iso /var/lib/vz/template/iso/
