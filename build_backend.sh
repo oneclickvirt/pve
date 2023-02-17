e0119824b1f77d0f72b5e06984c8cd398504b176
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-17 09:54:10 +0800

    Update build_backend.sh

--- build_backend.sh
+++ build_backend.sh
@@ -12,8 +12,22 @@ iface vmbr1 inet static
     bridge_stp off
     bridge_fd 0
 EOF
+if grep -q "iface vmbr1" /etc/network/interfaces; then
+    echo "Bridge vmbr1 is already in Proxmox VE configuration."
+else
+    # Add bridge configuration to Proxmox VE configuration
+    echo "Adding bridge vmbr1 to Proxmox VE configuration..."
+    cat <<EOF >> /etc/network/interfaces
+# Proxmox VE bridge vmbr1
+iface vmbr1 inet manual
+    bridge-ports enp0s8
+    bridge-stp off
+    bridge-fd 0
+EOF
+fi
 systemctl restart networking.service
 echo "Bridge vmbr1 created!"
+
 
 # 创建资源池
 echo "Creating resource pool mypool..."
