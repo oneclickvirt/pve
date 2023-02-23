c328ccdcd4ff689db7b39566dfbc801213883636
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-23 22:26:30 +0800

    Update network.sh

--- network.sh
+++ network.sh
@@ -4,20 +4,9 @@
 # 设置虚拟网桥名称
 BRIDGE_NAME="vmbr0"
 
-# 获取本地IPv4地址和子网掩码
+# 获取本地 IPv4地址 子网掩码 网关地址
 IP_ADDR=$(ip route get 8.8.8.8 | head -1 | awk '{print $7}')
 SUBNET_MASK=$(ip route get 8.8.8.8 | head -1 | awk '{print $1}' | awk -F"/" '{print $2}')
-
-# 计算网络地址
-IFS='.' read -ra IP_ARR <<< "$IP_ADDR"
-IFS='.' read -ra MASK_ARR <<< "$SUBNET_MASK"
-NET_ADDR=""
-for ((i=0; i<4; i++)); do
-  NET_ADDR="${NET_ADDR}$((${IP_ARR[$i]} & ${MASK_ARR[$i]}))."
-done
-NET_ADDR=${NET_ADDR::-1}
-
-# 获取网关地址
 GATEWAY=$(ip route | awk '/default/ {print $3}')
 
 # 创建虚拟网桥
