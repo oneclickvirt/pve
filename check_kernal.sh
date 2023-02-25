e30aee7c423bc790ca4bea919bb338a523bcea5b
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-25 22:19:45 +0800

    Update check_kernal.sh

--- check_kernal.sh
+++ check_kernal.sh
@@ -9,13 +9,13 @@ _blue() { echo -e "\033[36m\033[01m$@\033[0m"; }
 
 # 检查CPU是否支持硬件虚拟化
 if [ "$(egrep -c '(vmx|svm)' /proc/cpuinfo)" -eq 0 ]; then
-    _red "CPU不支持硬件虚拟化，无法嵌套虚拟化KVM服务器"
+    _red "CPU不支持硬件虚拟化，无法嵌套虚拟化KVM服务器，但可以开LXC服务器(CT)"
     exit 1
 fi
 
 # 检查虚拟化选项是否启用
 if [ "$(grep -E -c '(vmx|svm)' /proc/cpuinfo)" -eq 0 ]; then
-    _red "BIOS中未启用硬件虚拟化，无法嵌套虚拟化KVM服务器"
+    _red "BIOS中未启用硬件虚拟化，无法嵌套虚拟化KVM服务器，但可以开LXC服务器(CT)"
     exit 1
 fi
 
@@ -26,5 +26,5 @@ else
     # 加载KVM模块并将其添加到/etc/modules文件中
     modprobe kvm
     echo "kvm" >> /etc/modules
-    _green "KVM模块已加载并添加到 /etc/modules 可以使用PVE虚拟化KVM服务器"
+    _green "KVM模块已加载并添加到 /etc/modules 可以使用PVE虚拟化KVM服务器，也可以开LXC服务器(CT)"
 fi
