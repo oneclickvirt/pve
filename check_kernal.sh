486dc712d1f013aa2264dbe56d3012a152fd8c82
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-26 18:35:56 +0800

    Update check_kernal.sh

--- check_kernal.sh
+++ check_kernal.sh
@@ -27,12 +27,12 @@ fi
 if [ "$(cat /sys/module/kvm_intel/parameters/nested)" = "Y" ]; then
     if lsmod | grep -q kvm; then
         _green "本机系统支持KVM硬件嵌套虚拟化"
-        _green "本机符合要求：使用PVE虚拟化出来的KVM服务器可以在选项中开启KVM硬件虚拟化"
+        _green "本机符合要求：可以使用PVE虚拟化KVM服务器，并可以在开出来的KVM服务器选项中开启KVM硬件虚拟化"
     else
         _yellow "KVM模块未加载，不能使用PVE虚拟化KVM服务器，但可以开LXC服务器(CT)"
     fi
 else
-    _yellow "本机操作系统不支持KVM硬件嵌套虚拟化，使用PVE虚拟化出来的KVM服务器不能在选项中开启KVM硬件虚拟化"
+    _yellow "本机操作系统不支持KVM硬件嵌套虚拟化，使用PVE虚拟化出来的KVM服务器不能在选项中开启KVM硬件虚拟化，记得在开出来的KVM服务器选项中关闭"
 fi
 
 # 如果KVM模块未加载，则加载KVM模块并将其添加到/etc/modules文件中
