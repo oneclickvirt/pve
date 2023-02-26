56fed162869e2e64455d39baac26b23eb235e216
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-26 10:03:02 +0800

    Update check_kernal.sh

--- check_kernal.sh
+++ check_kernal.sh
@@ -19,6 +19,13 @@ if [ "$(grep -E -c '(vmx|svm)' /proc/cpuinfo)" -eq 0 ]; then
     exit 1
 fi
 
+# 查询系统是否支持
+if [ "$(cat /sys/module/kvm_intel/parameters/nested)" = "Y" ]; then
+    _green "本机操作系统支持KVM硬件嵌套虚拟化，使用PVE虚拟化出来的KVM服务器可以在选项中开启KVM硬件虚拟化"
+else
+    _yellow "本机操作系统不支持KVM硬件嵌套虚拟化，使用PVE虚拟化出来的KVM服务器不能在选项中开启KVM硬件虚拟化"
+fi
+
 # 检查KVM模块是否已加载
 if lsmod | grep -q kvm; then
     _green "KVM模块已经加载，可以使用PVE虚拟化KVM服务器"
