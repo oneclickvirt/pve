0b798e6cbe93a9dd5bc2433f1592f319e10cfccd
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-04-09 01:20:42 +0800

    Create rebuild.sh

---
+++ back/rebuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get install -y libguestfs-tools rng-tools wget curl
+curl -o rebuild_qcow2.sh https://raw.githubusercontent.com/spiritLHLS/pve/main/back/rebuild_qcow2.sh
+chmod 777 rebuild_qcow2.sh
+for image in ubuntu20.qcow2 ubuntu22.qcow2 QuTScloud_5.0.1.qcow2 almalinux8.qcow2 almalinux9.qcow2 alpinelinux_v3_15.qcow2 alpinelinux_v3_17.qcow2 archlinux.qcow2 centos7.qcow2 centos8-stream.qcow2 centos9-stream.qcow2 debian10.qcow2 debian11.qcow2 debian9.qcow2 fedora33.qcow2 fedora34.qcow2 opensuse-leap-15.qcow2 rockylinux8.qcow2 routeros_v6.qcow2 routeros_v7.qcow2 ubuntu18.qcow2; do
+  wget "https://cdn-backblaze.down.idc.wiki//Image/realServer-Template/$image"
+  chmod 777 $image
+done
+for image in ubuntu20.qcow2 ubuntu22.qcow2 QuTScloud_5.0.1.qcow2 almalinux8.qcow2 almalinux9.qcow2 alpinelinux_v3_15.qcow2 alpinelinux_v3_17.qcow2 archlinux.qcow2 centos7.qcow2 centos8-stream.qcow2 centos9-stream.qcow2 debian10.qcow2 debian11.qcow2 debian9.qcow2 fedora33.qcow2 fedora34.qcow2 opensuse-leap-15.qcow2 rockylinux8.qcow2 routeros_v6.qcow2 routeros_v7.qcow2 ubuntu18.qcow2; do
+  ./rebuild_qcow2.sh $image
+done
