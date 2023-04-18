51e6b678a73833b24a6bdccc5d44272b51d20a68
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-04-18 10:36:19 +0800

    Update rebuild.sh

--- back/rebuild.sh
+++ back/rebuild.sh
@@ -7,8 +7,8 @@ chmod 777 rebuild_qcow2.sh
 # QuTScloud_5.0.1.qcow2 fedora33.qcow2 fedora34.qcow2 opensuse-leap-15.qcow2 rockylinux8.qcow2 routeros_v6.qcow2 routeros_v7.qcow2
 # ubuntu20.qcow2 ubuntu22.qcow2 archlinux.qcow2 debian10.qcow2 debian11.qcow2 debian9.qcow2 ubuntu18.qcow2
 for image in almalinux8.qcow2 almalinux9.qcow2 alpinelinux_v3_15.qcow2 alpinelinux_v3_17.qcow2 centos7.qcow2 centos8-stream.qcow2 centos9-stream.qcow2; do
-#   curl -o $image "https://cdn-backblaze.down.idc.wiki//Image/realServer-Template/$image"
-  curl -o $image "https://github.com/spiritLHLS/Images/releases/download/v1.0/$image"
+  curl -o $image "https://cdn-backblaze.down.idc.wiki//Image/realServer-Template/$image"
+#   curl -o $image "https://github.com/spiritLHLS/Images/releases/download/v1.0/$image"
   chmod 777 $image
 done
 for image in almalinux8.qcow2 almalinux9.qcow2 alpinelinux_v3_15.qcow2 alpinelinux_v3_17.qcow2 centos7.qcow2 centos8-stream.qcow2 centos9-stream.qcow2; do
