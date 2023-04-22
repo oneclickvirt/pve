d3a80aed354c7b01d345c01bc60db8e240924533
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-04-22 19:33:39 +0800

    Update buildct.sh

--- back/buildct.sh
+++ back/buildct.sh
@@ -1,1 +1,2 @@
-pct create 100 local:vztmpl/debian-11-standard_11.6-1_amd64.tar.gz --cores 1 --cpuunits 1024 --memory 2048 --swap 128 --net0 name=eth0,ip=172.16.1.2/24,bridge=vmbr0,gw=172.16.1.1 --rootfs local:24 --onboot 1
+pct create 100 local:vztmpl/debian-11-standard_11.6-1_amd64.tar.zst --cores 1 --cpuunits 1024 --memory 2048 --swap 128 --net0 name=eth0,ip=172.16.1.2/24,bridge=vmbr1,gw=172.16.1.1 --rootfs local:10 --onboot 1
+
