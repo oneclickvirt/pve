2861022ea12bd44a0ac735fc94383861c8deb3ba
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-04-22 20:27:06 +0800

    Update buildct.sh

--- back/buildct.sh
+++ back/buildct.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # from
 # https://github.com/spiritLHLS/pve
-# 2023.04.17
+# 2023.04.22
+
+# ./buildct.sh CTID 密码 CPU核数 内存 硬盘 SSH端口 80端口 443端口 外网端口起 外网端口止 系统
+# ./buildct.sh 102 1234567 1 512 5 40001 40002 40003 50000 50025 debian11
 
 cd /root >/dev/null 2>&1
-# 创建容器
 CTID="${1:-102}"
 password="${2:-123456}"
 core="${3:-1}"
@@ -15,12 +17,41 @@ web1_port="${7:-40002}"
 web2_port="${8:-40003}"
 port_first="${9:-49975}"
 port_last="${10:-50000}"
-system="${12:-debian10}"
+system="${12:-debian11}"
 rm -rf "ct$name"
 TMP_FILE="$(mktemp)"
 echo "#cloud-config" > "$TMP_FILE"
 echo "chpasswd:" >> "$TMP_FILE"
 echo "  list: |" >> "$TMP_FILE"
 echo "    root:$password" >> "$TMP_FILE"
-pct create $CTID local:vztmpl/debian-11-standard_11.6-1_amd64.tar.zst --cores 1 --cpuunits 1024 --memory 2048 --swap 128 --net0 name=eth0,ip=172.16.1.2/24,bridge=vmbr1,gw=172.16.1.1 --rootfs local:10 --onboot 1
+system="debian-11-standard_11.6-1_amd64.tar.zst"
+
+first_digit=${CTID:0:1}
+second_digit=${CTID:1:1}
+third_digit=${CTID:2:1}
+if [ $first_digit -le 2 ]; then
+  if [ $second_digit -eq 0 ]; then
+    num=$third_digit
+  else
+    num=$second_digit$third_digit
+  fi
+else
+  num=$((first_digit - 2))$second_digit$third_digit
+fi
+user_ip="172.16.1.${num}"
+pct create $CTID local:vztmpl/$system --cores $core --cpuunits 1024 --memory $memory --swap 128 --net0 name=eth0,ip=${user_ip}/24,bridge=vmbr1,gw=172.16.1.1 --rootfs local:${disk} --onboot 1 --cloudinit /dev/stdin < "$TMP_FILE"
 rm "$TMP_FILE"
+pct start $CTID
+
+iptables -t nat -A PREROUTING -p tcp --dport ${sshn} -j DNAT --to-destination ${user_ip}:22
+iptables -t nat -A PREROUTING -p tcp -m tcp --dport ${web1_port} -j DNAT --to-destination ${user_ip}:80
+iptables -t nat -A PREROUTING -p tcp -m tcp --dport ${web2_port} -j DNAT --to-destination ${user_ip}:443
+iptables -t nat -A PREROUTING -p tcp -m tcp --dport ${port_first}:${port_last} -j DNAT --to-destination ${user_ip}:${port_first}-${port_last}
+iptables -t nat -A PREROUTING -p udp -m udp --dport ${port_first}:${port_last} -j DNAT --to-destination ${user_ip}:${port_first}-${port_last}
+if [ ! -f "/etc/iptables/rules.v4" ]; then
+    touch /etc/iptables/rules.v4
+fi
+iptables-save > /etc/iptables/rules.v4
+service netfilter-persistent restart
+echo "$CTID $password $core $memory $disk $sshn $web1_port $web2_port $port_first $port_last $system" >> "ct${vm_num}"
+cat "ct${vm_num}"
