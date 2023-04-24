6cede4d382e2ac2bad1b5cf275a25b8454d1946e
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-04-24 08:35:22 +0800

    Update buildct.sh

--- scripts/buildct.sh
+++ scripts/buildct.sh
@@ -11,6 +11,9 @@ _red() { echo -e "\033[31m\033[01m$@\033[0m"; }
 _green() { echo -e "\033[32m\033[01m$@\033[0m"; }
 _yellow() { echo -e "\033[33m\033[01m$@\033[0m"; }
 _blue() { echo -e "\033[36m\033[01m$@\033[0m"; }
+export LC_ALL=en_US.utf8
+export LANG=en_US.utf8
+
 
 cd /root >/dev/null 2>&1
 CTID="${1:-102}"
