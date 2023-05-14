6a068042556665abb6ecf5a22852bdde82d33919
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-05-14 08:59:34 +0800

    Update check-dns.sh

--- scripts/check-dns.sh
+++ scripts/check-dns.sh
@@ -10,5 +10,5 @@ if [ $? -eq 0 ]; then
     echo "DNS server ${DNS_SERVER} already exists in ${RESOLV_CONF}."
 else
     echo "Adding DNS server ${DNS_SERVER} to ${RESOLV_CONF}..."
-    echo "nameserver ${DNS_SERVER}" >> ${RESOLV_CONF}
+    echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8\nnameserver 8.8.4.4\nnameserver 2606:4700:4700::1111\nnameserver 2001:4860:4860::8888\nnameserver 2001:4860:4860::8844" >> ${RESOLV_CONF}
 fi
