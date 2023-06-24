5aa07aa303fa82543d9e98c5af9f5bc355a5403f
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-06-24 10:12:43 +0800

    Update install_pve.sh

--- scripts/install_pve.sh
+++ scripts/install_pve.sh
@@ -355,7 +355,8 @@ fi
 apt-get install gnupg -y
 if [ $(uname -m) != "x86_64" ] || [ ! -f /etc/debian_version ] || [ $(grep MemTotal /proc/meminfo | awk '{print $2}') -lt 2000000 ] || [ $(grep -c ^processor /proc/cpuinfo) -lt 2 ] || [ $(ping -c 3 google.com > /dev/null 2>&1; echo $?) -ne 0 ]; then
   _red "Error: This system does not meet the minimum requirements for Proxmox VE installation."
-  reading "是否要继续安装(非Debian系或不满足最低的配置安装要求会爆上面这个警告)？(回车则默认不继续安装) [y/n] " confirm
+  _yellow "Do you want to continue the installation? (Enter to not continue the installation by default) (y/[n])"
+  reading "是否要继续安装？(回车则默认不继续安装) (y/[n]) " confirm
   echo ""
   if [ "$confirm" != "y" ]; then
     exit 1
