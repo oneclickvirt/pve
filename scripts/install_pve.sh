1eaadd9866e3da60abd32ca47aa0a8b2455467e7
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-07-28 18:39:20 +0800

    Update install_pve.sh

--- scripts/install_pve.sh
+++ scripts/install_pve.sh
@@ -718,6 +718,7 @@ _green "Running kernel: $(pveversion)"
 installed_kernels=($(dpkg -l 'pve-kernel-*' | awk '/^ii/ {print $2}' | cut -d'-' -f3- | sort -V))
 latest_kernel=${installed_kernels[-1]}
 _green "PVE latest kernel: $latest_kernel"
+chattr +i /etc/network/interfaces
 if [ ! -s "/etc/resolv.conf" ]
 then
     cp /etc/resolv.conf /etc/resolv.conf.bak
