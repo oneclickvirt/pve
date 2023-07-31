66159fd21082a9de02f7c6e1f837b8c0355bd90f
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-07-31 12:47:35 +0800

    Update install_pve.sh

--- scripts/install_pve.sh
+++ scripts/install_pve.sh
@@ -698,12 +698,13 @@ if [ ! -f "/usr/local/bin/reboot_pve.txt" ]; then
         sed -i 's#http://debian-archive.trafficmanager.net/debian-security#http://security.debian.org/debian-security#g' /etc/apt/sources.list
         sed -i 's#http://debian-archive.trafficmanager.net/debian bullseye-updates#http://deb.debian.org/debian bullseye-updates#g' /etc/apt/sources.list
         sed -i 's#http://debian-archive.trafficmanager.net/debian bullseye-backports#http://deb.debian.org/debian bullseye-backports#g' /etc/apt/sources.list
+        # if [[ "${inet_dhcp}" == true ]]; then
+        #     # 特殊处理原有配置是dhcp的情况
+        #     prebuild_ifupdown2
+        # fi
     fi
     if [[ $output == *"Hetzner_vServer"* ]]; then
         # 特殊处理Hetzner
-        prebuild_ifupdown2
-    elif [[ "${inet_dhcp}" == true ]]; then
-        # 特殊处理原有配置是dhcp的情况
         prebuild_ifupdown2
     fi
     # # 特殊处理OVH
