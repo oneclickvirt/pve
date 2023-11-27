c004c353c47c306361e11e9f4867a0657c59f26b
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-11-27 03:59:21 +0000

    2023.11.26

--- scripts/build_nat_network.sh
+++ scripts/build_nat_network.sh
@@ -214,6 +214,7 @@ else
         fi
         ipv6_address_without_last_segment="${ipv6_address%:*}:"
         if ping -c 1 -6 -W 3 $ipv6_address >/dev/null 2>&1; then
+            check_ipv6
             echo "${ipv6_address}" >/usr/local/bin/pve_check_ipv6
         fi
     fi
